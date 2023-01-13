2b5d3308f78d374e1a2f6d186b1034c85e97aac3
AbbasiMohamad <m.abbasi.72@gmail.com>
2023-01-14 00:30:40 +0330

    feat: add selection sort algorithm

---
+++ src/Algorithms.Implementations/SelectionSortAlgorithm/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Implementations.SelectionSortAlgorithm;
+
+public class SelectionSort
+{
+
+    public int[] SortBySelection(int[] arrays)
+    {
+        int[] newarray = new int[arrays.Length];
+        
+        for (int i=0; i < newarray.Length; i++)
+        {
+            newarray[i] = FindSmallest(arrays);
+            int index = Array.IndexOf(arrays, newarray[i]);
+            arrays = arrays.Where((val, i) => i != index).ToArray();
+        }
+        return newarray;
+    }
+
+    // [4,7,2,8]
+    private int FindSmallest(int[] array)
+    {
+        int lowestValue = array[0];
+        for (int i=0; i < array.Length; i++)
+        {
+            if (lowestValue > array[i])
+            {
+                lowestValue = array[i];
+            }
+        }
+        return lowestValue;
+    }
+}
