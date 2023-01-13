c5908ac142cdaf5112d5c892f8fbd5cfab3edd83
AbbasiMohamad <m.abbasi.72@gmail.com>
2023-01-14 00:30:40 +0330

    feat: add selection sort algorithm

--- test/Algorithms.ImplementationsTest/BinarySearchAlgorithm/BinarySearchTest.cs
+++ test/Algorithms.ImplementationsTest/BinarySearchAlgorithm/BinarySearchTest.cs
@@ -9,7 +9,7 @@ public class BinarySearchTest
     [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 5, 4)]
     [InlineData(new[] { 1, 4, 6, 7, 11, 16, 19, 28 }, 19, 6)]
     [InlineData(new[] { 1, 4, 6, 7, 11, 16, 19, 28 }, 30, null)]
-    public void test_in_binary_search(int[] sortedList, int targetItem, int? expectedResult)
+    public void test_binary_search(int[] sortedList, int targetItem, int? expectedResult)
     {
         var mut = new BinarySearch();
         var actualResult = mut.SearchByBinary(sortedList, targetItem);
---
+++ test/Algorithms.ImplementationsTest/SelectionSortAlgorithm/SelectionSortTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Implementations.SelectionSortAlgorithm;
+
+namespace Algorithms.ImplementationsTest.BinarySearchAlgorithm;
+
+public class SelectionSortTest
+{
+
+    [Theory]
+    [InlineData(new[] { 7, 2, 6, 4, 5, 3, 1, 8 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+    [InlineData(new[] { 1, 19, 6, 28, 11, 16, 4, 7 }, new[] { 1, 4, 6, 7, 11, 16, 19, 28 })]
+    [InlineData(new[] { 28, 1, 4, 6, 7, 11, 16, 19 }, new[] { 1, 4, 6, 7, 11, 16, 19, 28 })]
+    [InlineData(new[] { 28, 1, -4, 6, 7, -11, 16, -19 }, new[] { -19, -11, -4, 1, 6, 7, 16, 28 })]
+    public void test_selection_sort(int[] array, int[] sortedArray)
+    {
+        var mut = new SelectionSort();
+        var actualResult = mut.SortBySelection(array);
+        Assert.Equal(sortedArray, actualResult);
+    }
+}
